a440ddec0df2bc620aa440d8f43a51a27d513c93
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-10-11 09:42:57 -0700

    code

--- PythonSolution/anotherfile.py
+++ PythonSolution/anotherfile.py
@@ -1,3 +1,15 @@
 import sys
 print(sys.argv[0])
 print("Hello, World2!")
+x = int(input("enter your number:"))
+print("you entered", x)
+myarray=[0,1,2]
+print(len(myarray))
+something = {}
+something["pepe"]=1
+something["nina"]=2
+print(something["pepe"])
+name1, age1 = something.items()
+for name, age in something.items():
+    print(name,age)
+range(10)
